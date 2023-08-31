8f4ff76ee7e583849e23436a0949bc96cca0d8c4
Filip Merta <filmer.projects@gmail.com>
2023-08-31 11:12:18 +0200

    Add - Password record class

---
+++ db/password_record.py
@@ -0,0 +1,9 @@
+class PasswordRecord:
+    def __init__(self, url, username, password, date, notes) -> None:
+        self.url = url
+        self.username = username
+        self.password = password
+        self.date = date
+        self.notes = notes
+
+    
