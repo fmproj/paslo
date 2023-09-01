af4c238f3105fe3840ed6256944a478f4b482e2b
Filip Merta <filmer.projects@gmail.com>
2023-09-01 10:41:50 +0200

    Refac - Move db conn from methods to file global

--- db/database.py
+++ db/database.py
@@ -7,7 +7,9 @@ def open_db(path) -> sqlite3.Connection:
     conn = sqlite3.connect(path)
     return conn
 
-def create_passrecs_table(conn) -> None:
+CONN = open_db(os.path.join(os.path.dirname(os.path.abspath(__file__)), "paslo.db"))
+
+def create_passrecs_table() -> None:
     create_passrecs_query = '''
         CREATE TABLE IF NOT EXISTS password_records (
             id INTEGER PRIMARY KEY,
@@ -19,13 +21,15 @@ def create_passrecs_table(conn) -> None:
         )
     '''
 
-    cursor = conn.cursor()
+    cursor = CONN.cursor()
     cursor.execute(create_passrecs_query)
-    conn.commit()
+    CONN.commit()
 
-def add_passrec(conn: sqlite3.Connection, url, username, password, notes) -> int:
+create_passrecs_table()
+
+def add_passrec(url, username, password, notes) -> int:
     try:
-        cursor = conn.cursor()
+        cursor = CONN.cursor()
         date = datetime.datetime.now()
         insert_query = '''
             INSERT INTO password_records (url, username, password, updated, notes)
@@ -33,7 +37,7 @@ def add_passrec(conn: sqlite3.Connection, url, username, password, notes) -> int
         '''
 
         cursor.execute(insert_query, (url, username, hash_password(password), date, notes))
-        conn.commit()
+        CONN.commit()
         return cursor.lastrowid
     except sqlite3.Error as e:
         # TODO: Log the error
@@ -44,9 +48,9 @@ def hash_password(password: str):
     hashed_pass = bcrypt.hashpw(password.encode("utf-8"), salt)
     return hashed_pass
 
-def get_passrec(conn: sqlite3.Connection, url) -> list[any]:
+def get_passrec(url) -> list[any]:
     try:
-        cursor = conn.cursor()
+        cursor = CONN.cursor()
         get_query = '''SELECT * FROM password_records WHERE url = ?'''
 
         cursor.execute(get_query, (url,))
@@ -55,9 +59,9 @@ def get_passrec(conn: sqlite3.Connection, url) -> list[any]:
         # TODO: Log the error
         return []
 
-def update_passrec(conn: sqlite3.Connection, url, username=None, password=None, notes=None) -> None:
+def update_passrec(url, username=None, password=None, notes=None) -> None:
     try:
-        cursor = conn.cursor()
+        cursor = CONN.cursor()
         date = datetime.datetime.now()
         update_query = '''
             UPDATE password_records
@@ -66,34 +70,27 @@ def update_passrec(conn: sqlite3.Connection, url, username=None, password=None, 
         '''
         
         cursor.execute(update_query, (username, password, date, notes, url))
-        conn.commit()
+        CONN.commit()
     except sqlite3.Error as e:
         # TODO: Log the error
         pass
 
-def delete_password(conn: sqlite3.Connection, url) -> None:
+def delete_password(url) -> None:
     try:
-        cursor = conn.cursor()
+        cursor = CONN.cursor()
         delete_query = '''DELETE FROM password_records WHERE url = ?'''
 
         cursor.execute(delete_query, (url,))
-        conn.commit()
+        CONN.commit()
     except sqlite3.Error as e:
         # TODO: Log the error
         pass
 
-if __name__ == "__main__":
-    self_dir = os.path.dirname(os.path.abspath(__file__))
-    db_name = os.path.join(self_dir, "paslo.db")
+if __name__ == "__main__": 
+    # print(add_passrec("LOL", "user", "heslo", "Login for LOL game"))
+    # print(get_passrec("LOL"))
+    # update_passrec("LOL", username="admin", notes="This is changed LOL note")
+    # print(get_passrec("LOL"))
+    # delete_password("LOL")
 
-    conn = open_db(db_name)
-    create_passrecs_table(conn)
-
-    print(add_passrec(conn, "LOL", "user", "heslo", "Login for LOL game"))
-    print(get_passrec(conn, "LOL"))
-    update_passrec(conn, "LOL", username="admin", notes="This is changed LOL note")
-    print(get_passrec(conn, "LOL"))
-    delete_password(conn, "LOL")
-    print(get_passrec(conn, "LOL"))
-
-    conn.close()
+    CONN.close()
